d05a832504e400848aff1f573b34c5031db20f0c
HamzaAli <hamza.ali@upgenicsint.work>
2021-06-01 16:57:56 +0500

    add static method via class name remove toast

--- PhoneCheckLib/src/main/java/com/Upgenics/phonechecklib/FileManger.kt
+++ PhoneCheckLib/src/main/java/com/Upgenics/phonechecklib/FileManger.kt
@@ -10,7 +10,6 @@ class FileManger() {
    companion object {
        fun helloUpgenics(context: Context) {
            Log.d("PhoneChekTag", "Lib implimented")
-           Toast.makeText(context, "dev from PhoneCheck Upgenics int.", Toast.LENGTH_SHORT).show()
        }
    }
 
