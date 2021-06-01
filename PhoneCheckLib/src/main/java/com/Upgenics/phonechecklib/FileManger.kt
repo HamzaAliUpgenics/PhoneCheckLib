783e731d2b55a9b769bf42eec7af3bd2571df28e
HamzaAli <hamza.ali@upgenicsint.work>
2021-06-01 13:12:43 +0500

    add static method via class name

--- PhoneCheckLib/src/main/java/com/Upgenics/phonechecklib/FileManger.kt
+++ PhoneCheckLib/src/main/java/com/Upgenics/phonechecklib/FileManger.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import android.widget.Toast
 
 class FileManger() {
+   @JvmField
    var fileName = "PhoneCheckUpgenics_SampleLib"
-   fun  helloUpgenics(context: Context){
-       Log.d("PhoneChekTag","Lib implimented")
-       Toast.makeText(context,"dev from PhoneCheck Upgenics int.",Toast.LENGTH_SHORT).show()
-     }
+   companion object {
+       fun helloUpgenics(context: Context) {
+           Log.d("PhoneChekTag", "Lib implimented")
+           Toast.makeText(context, "dev from PhoneCheck Upgenics int.", Toast.LENGTH_SHORT).show()
+       }
+   }
+
 }
